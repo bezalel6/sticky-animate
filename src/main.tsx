6613a2ae08a36a0b767be059b1edcc30e2f2c5f3
bezalel6 <bezalel3250@gmail.com>
2025-04-24 10:56:50 +0300

    Revert "Fully working animation on media query"
    This reverts commit ac10de89d1c6bf05cfca0f7ef4ad8afbec3b7c19.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import { scan } from "react-scan";
-scan({ enabled: true });
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
